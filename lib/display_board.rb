5988d7f52724a7bd8807b1a4635c747584f25506
Daniel Kim <daniel.kim52@gmail.com>
2019-04-27 09:06:58 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -3,6 +3,6 @@
 board = [" X ", " X ", " X "," O ", " ", " ", " ", " ", " "]
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
-  puts "------------------"
+  puts "-----------------"
 end
 display_board(board)
